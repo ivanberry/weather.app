9c5b9697ad89628622f4b689547de0fa99d18ee6
ivanberry <ivansync@outlook.com>
2016-05-28 17:28:18 +0800

    优化html结构

--- gulpfile.js
+++ gulpfile.js
@@ -11,12 +11,12 @@ const reload = browserSync.reload;
 gulp.task('styles', () => {
   return gulp.src('app/styles/*.scss')
     .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
+    //.pipe($.sourcemaps.init())
+    //.pipe($.sass.sync({
+     // outputStyle: 'expanded',
+      //precision: 10,
+      //includePaths: ['.']
+    //i}).on('error', $.sass.logError))
     .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
